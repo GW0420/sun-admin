4294484d9ed0ee7390b000efa1efa38f8a199353
“wei <116019284@qq.com>
2023-02-19 18:49:17 +0800

    feat: 项目初始化

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import './style/reset.css'
+import ElementPlus from './util/element'
+
+const app = createApp(App)
+ElementPlus(app)
+app.use(store)
+app.use(router)
+app.mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="home">home</div>
+  <div class="home">
+    <el-row class="mb-4">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
+    <div style="font-size: 20px">
+      <!-- 由于SVG图标默认不携带任何属性 -->
+      <!-- 你需要直接提供它们 -->
+      <Edit style="width: 1em; height: 1em; margin-right: 8px" />
+      <Share style="width: 1em; height: 1em; margin-right: 8px" />
+      <Delete style="width: 1em; height: 1em; margin-right: 8px" />
+      <Search style="width: 1em; height: 1em; margin-right: 8px" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
