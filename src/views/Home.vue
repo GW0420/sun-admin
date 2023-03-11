68ae877e5c8f0ae045477178d8ff209df2213377
sun <116019284@qq.com>
2023-03-11 17:11:14 +0800

    feat: 目录搭建

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <el-row class="mb-4">
       <el-button>Default</el-button>
-      <el-button type="primary">Primary</el-button>
+      <el-button type="primary">Primary1</el-button>
       <el-button type="success">Success</el-button>
       <el-button type="info">Info</el-button>
       <el-button type="warning">Warning</el-button>
